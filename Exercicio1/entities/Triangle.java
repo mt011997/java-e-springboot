e2abea9d80de8fc668bb55429c93c28880fdf9bc
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-10-01 23:26:05 -0300

    ident

--- Exercicio1/entities/Triangle.java
+++ Exercicio1/entities/Triangle.java
@@ -1,12 +1,12 @@
-package Exercicio1.entities;  //Pacote da classe
+package Exercicio1.entities; //Pacote da classe
 
 public class Triangle { // Nome da classe
 
   public double a; //
   public double b; // Atributos da Classe
   public double c; //
-  
-  public double area(){
+
+  public double area() {
     double p = (a + b + c) / 2.0;
     return Math.sqrt(p * (p - a) * (p - b) * (p - c));
   }
