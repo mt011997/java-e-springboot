c157eddf0c093115360b0a5469a8703490bd374f
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-10-01 23:26:38 -0300

    ident

--- Exercicio3/entities/Rectangle.java
+++ Exercicio3/entities/Rectangle.java
@@ -4,19 +4,20 @@ public class Rectangle {
   public double width;
   public double height;
 
-  public double area(){
+  public double area() {
     return width * height;
   }
 
-  public double perimeter(){
+  public double perimeter() {
     return 2 * (width + height);
   }
 
-  public double diagonal(){
+  public double diagonal() {
     return Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
   }
 
-  public String toString(){
-    return "Area = " + String.format("%.2f%n", area()) + "Perímetro = " + String.format("%.2f%n", perimeter()) + "Diagonal = " + String.format("%.2f%n", diagonal());
+  public String toString() {
+    return "Area = " + String.format("%.2f%n", area()) + "Perímetro = " + String.format("%.2f%n", perimeter())
+        + "Diagonal = " + String.format("%.2f%n", diagonal());
   }
 }
