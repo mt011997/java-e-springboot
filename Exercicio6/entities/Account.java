712feb412895b4438dc76117b313f6324a9b868b
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-10-06 19:28:37 -0300

    entitie

--- Exercicio6/entities/Account.java
+++ Exercicio6/entities/Account.java
@@ -5,18 +5,19 @@ public class Account {
   private String holder;
   private double balance;
   
-  public Account(int number, String holder, double balance) {
+  public Account(int number, String holder) {
     this.number = number;
     this.holder = holder;
-    this.balance = balance;
+  }
+
+    public Account(int number, String holder, double initialDeposit) {
+    this.number = number;
+    this.holder = holder;
+    deposit(initialDeposit);
   }
 
   public int getNumber() {
     return number;
-  }
-
-  public void setNumber(int number) {
-    this.number = number;
   }
 
   public String getHolder() {
@@ -31,9 +32,21 @@ public class Account {
     return balance;
   }
 
-  public void setBalance(double balance) {
-    this.balance = balance;
+  public void deposit(double amount){
+    this.balance += amount;
   }
 
+  public void withdraw(double amount){
+    this.balance -= amount + 5.0;
+  }
+
+  public String toString() {
+		return "Account "
+				+ number
+				+ ", Holder: "
+				+ holder
+				+ ", Balance: $ "
+				+ String.format("%.2f", balance);
+	}
   
 }
